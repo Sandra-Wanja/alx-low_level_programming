4d319d7afef067902775456aa06920b0a70f57fb
Sandra-Wanja <sandrawanja8@gmail.com>
2023-07-14 12:00:33 -0700

    c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,13 +3,14 @@
 /**
  * main - Entry point
  *
+ * int i;
+ * double d;
+ * char c;
+ *
  * Return : Always 0 (success)
  */
 int main(void)
 {
-	int i;
-	double d;
-	char c;
 
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
