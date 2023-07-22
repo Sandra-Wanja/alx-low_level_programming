8ede1fca031221f2bb3f0b9d47253b8ab4dd7bbf
Sandra-Wanja <sandrawanja8@gmail.com>
2023-07-22 16:00:07 -0700

    shows value of integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolute value of an integer
+ * @i: input number as integer
+ *
+ * Return: absolute value of the integer
+ */
+int _abs(int)
+{
+	if (i >= '0')
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
