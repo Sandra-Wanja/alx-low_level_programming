f98c58fbde44ff5bf12878f74cbfffb016886a7b
Sandra-Wanja <sandrawanja8@gmail.com>
2023-07-27 15:12:16 -0700

    encodes a string

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * rot13 - encodes a string
+ * @str: input string to be encoded
+ *
+ * Return: pointer to encoded string
+ */
+char *rot13(char *str)
+{
+	char *input = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *output = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; input[j] != '\0'; j++)
+		{
+			if (str[i] == input[j])
+			{
+				str[i] = output[j];
+				break;
+			}
+		}
+	}
+
+	return str;
+}
