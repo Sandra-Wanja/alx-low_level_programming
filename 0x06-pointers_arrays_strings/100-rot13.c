af96cab173383dfee2e81fc12f129e83461534e6
Sandra-Wanja <sandrawanja8@gmail.com>
2023-07-29 01:58:55 -0700

    encodes string

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,29 +1,44 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 
 /**
- * rot13 - encodes a string
- * @str: input string to be encoded
- *
- * Return: pointer to encoded string
- */
+* rot13 -  encodes a string
+* @str: string targeted
+*Return: returns  encoded string
+*/
+
 char *rot13(char *str)
 {
-	char *input = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char *output = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-	int i, j;
+	int index1, index2;
 
-	for (i = 0; str[i] != '\0'; i++)
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+											 'G', 'H', 'I', 'J', 'K', 'L',
+											 'M', 'N', 'O', 'P', 'Q', 'R',
+											 'S', 'T', 'U', 'V', 'W', 'X',
+											 'Y', 'Z', 'a', 'b', 'c', 'd',
+											 'e', 'f', 'g', 'h', 'i', 'j',
+											 'k', 'l', 'm', 'n', 'o', 'p',
+											 'q', 'r', 's', 't', 'u', 'v',
+											 'w', 'x', 'y', 'z'};
+
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+											 'T', 'U', 'V', 'W', 'X', 'Y',
+											 'Z', 'A', 'B', 'C', 'D', 'E',
+											 'F', 'G', 'H', 'I', 'J', 'K',
+											 'L', 'M', 'n', 'o', 'p', 'q',
+											 'r', 's', 't', 'u', 'v', 'w',
+											 'x', 'y', 'z', 'a', 'b', 'c',
+											 'd', 'e', 'f', 'g', 'h', 'i',
+											 'j', 'k', 'l', 'm'};
+	while (str[++index1])
 	{
-		for (j = 0; input[j] != '\0'; j++)
+		for (index2 = 0; index2 < 52; index2++)
 		{
-			if (str[i] == input[j])
+			if (str[index1] == alphabet[index2])
 			{
-				str[i] = output[j];
+				str[index1] = rot13key[index2];
 				break;
 			}
 		}
 	}
-
 	return (str);
 }
