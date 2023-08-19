a5f56f7709f0c5fab072f8bf88013830e89059da
Sandra-Wanja <sandrawanja8@gmail.com>
2023-08-19 03:39:01 -0700

    computes absolute value number

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef _MY_HEADER_
-#define _MY_HEADER_
+#ifndef _FUNCTION_LIKE_MACRO_
+#define _FUNCTION_LIKE_MACRO_
 
-#define ABS(x) ( (x) < 0 ? ( (x) * -1): (x)
+#define ABS(x) ((x) < (0) ? ((x) * (-1)) : (x))
 
 #endif
