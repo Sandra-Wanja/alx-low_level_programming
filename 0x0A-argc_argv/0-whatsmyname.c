347443b1c31b2d0053692c9dddfab36cd8680a95
Sandra-Wanja <sandrawanja8@gmail.com>
2023-09-06 10:49:56 -0700

    back to printf

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - prints its name
@@ -11,7 +12,7 @@ int main(int argc, char *argv[])
 {
 	(void) argc;
 
-	_putchar("%s\n", argv[0]);
+	_printf("%s\n", argv[0]);
 
 	return (0);
 }
