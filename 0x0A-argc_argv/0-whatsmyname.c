edb0ae69a68fc3c1d0d9e47495e022d016573fc1
Sandra-Wanja <sandrawanja8@gmail.com>
2023-09-06 10:42:33 -0700

    changed from to printf

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -11,7 +12,7 @@ int main(int argc, char *argv[])
 {
 	(void) argc;
 
-	_putchar("%s\n", argv[0]);
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
