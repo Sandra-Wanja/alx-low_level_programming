3fe2cb330d51b3d6ab4bd0165d47c92cdc732433
Sandra-Wanja <sandrawanja8@gmail.com>
2023-09-11 14:31:49 -0700

    typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,4 @@
-#ifndef _MAIN_H
+#ifndef _MAIN_H_
 #define _MAIN_H_
 
 /**
@@ -16,14 +16,13 @@ struct dog
 	char *owner;
 };
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-
 /**
  * dog_t - new name for struct dog
  */
 typedef struct dog dog_t;
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
