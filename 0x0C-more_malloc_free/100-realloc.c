bc6db810fe1333fb7fb86c44f1f5869df15774dc
Sandra-Wanja <sandrawanja8@gmail.com>
2023-09-12 12:15:16 -0700

    connects two string

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _realloc -  reallocates a memory block 
+ * @ptr: pointer of previous memory
+ * @old_size: size of ptr
+ * @new_size: size of new memory
+ *
+ * Return:  
