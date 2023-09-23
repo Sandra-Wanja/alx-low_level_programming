a940775201ca8dadcdaac7e7b4ecd734e6f1e68b
Sandra-Wanja <sandrawanja8@gmail.com>
2023-09-23 12:00:13 -0700

    prints list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,9 +8,7 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t num;
-
-	num = 0;
+	size_t num = 0;
 
 	while (h != NULL)
 	{
@@ -18,10 +16,8 @@ size_t print_list(const list_t *h)
 			printf("[%d] %s\n", 0, "(nil)");
 		else
 			printf("[%d] %s\n", h->len, h->str);
-
 		h = h->next;
 		num++;
 	}
-
 	return (num);
 }
