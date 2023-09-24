82431bca38532eaa6f4b5c2e1bff834f5364e2df
Sandra-Wanja <sandrawanja8@gmail.com>
2023-09-24 11:00:04 -0700

    free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - free singly linked list
+ * @head: pointer to first node of linked
+ * list
+ *
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	list_t *ptr;
+
+	while ((current = head) != NULL)
+	{
+		head = head->next;
+		free(ptr->str);
+		free(ptr);
+	}
+}
