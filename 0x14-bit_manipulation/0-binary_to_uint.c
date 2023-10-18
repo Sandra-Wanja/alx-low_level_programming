8c1380d44d21c2d9a4a7f6366b724097cac59425
Sandra-Wanja <sandrawanja8@gmail.com>
2023-10-18 06:26:25 -0700

    bit man

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,34 +1,36 @@
 #include "main.h"
 
 /**
- * binary_to_uint - binary to unsigned int
- * @b: pointer to binary array
+ * binary_to_uint - convert binary to int
+ * @b: binary num
  *
- * Return: unsigned integer value
+ * Return: int value
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int h;
-	int n, power_two;
+	unsigned int i;
+	int ln, base_two;
 
 	if (!b)
 		return (0);
 
-	h = 0;
+	i = 0;
 
-	for (n = 0; b[n] != '\0'; n++)
+	for (ln = 0; b[ln] != '\0'; ln++)
 		;
-	for (n--, power_two = 1; n >= 0; n--, power_two *= 2)
+
+	for (ln--, base_two = 1; ln >= 0; ln--, base_two *= 2)
 	{
-		if (b[n] != '0' && b[n] != '1')
+		if (b[ln] != '0' && b[ln] != '1')
 		{
 			return (0);
 		}
-		if (b[n] & 1)
+
+		if (b[ln] & 1)
 		{
-			h += power_two;
+			i += base_two;
 		}
 	}
 
-	return (h);
+	return (i);
 }
